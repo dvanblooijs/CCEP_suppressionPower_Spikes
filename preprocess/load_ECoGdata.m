9391d73b7e44aece220908e6d3410925c857fef9
Dorien van Blooijs <d.vanblooijs@umcutrecht.nl>
2019-09-26 18:05:45 +0200

    add files for running pipeline

---
+++ preprocess/load_ECoGdata.m
@@ -0,0 +1,71 @@
+% function to read data into dataBase
+% author: Dorien van Blooijs
+% date: June 2019
+
+function dataBase = load_ECoGdata(cfg)
+
+dataPath = cfg.dataPath;
+sub_labels = cfg.sub_labels;
+ses_label = cfg.ses_label;
+task_label = cfg.task_label;
+
+dataBase = struct([]);
+for i=1:size(sub_labels,2)
+    D = dir(fullfile(dataPath,[ sub_labels{i}],ses_label,'ieeg',...
+        [sub_labels{i} '_' ses_label '_' task_label ,'_run-*_ieeg.eeg']));
+    
+    % determin run_label
+    run_label = D(1).name(strfind(D(1).name,'run-'):strfind(D(1).name,'_ieeg')-1);
+    
+    dataName = fullfile(D(1).folder, D(1).name);
+    
+    ccep_data = ft_read_data(dataName,'dataformat','brainvision_eeg');
+    ccep_header = ft_read_header(dataName);
+    
+    % load events
+    D = dir(fullfile(dataPath,[sub_labels{i}],ses_label,'ieeg',...
+        [sub_labels{i} '_' ses_label '_' task_label ,'_run-*_events.tsv']));
+    
+    eventsName = fullfile(D(1).folder, D(1).name);
+    
+    tb_events = readtable(eventsName,'FileType','text','Delimiter','\t');
+    
+    % load electrodes
+    D = dir(fullfile(dataPath,[sub_labels{i}],ses_label,'ieeg',...
+        [sub_labels{i} '_' ses_label ,'_electrodes.tsv']));
+    
+    elecsName = fullfile(D(1).folder, D(1).name);
+    
+    tb_electrodes = readtable(elecsName,'FileType','text','Delimiter','\t');
+    log_elec_incl = ~strcmp(tb_electrodes.group,'other');
+    tb_electrodes = tb_electrodes(log_elec_incl,:);
+    
+    % load channels
+    D = dir(fullfile(dataPath,[sub_labels{i}], ses_label,'ieeg',...
+        [sub_labels{i} '_' ses_label '_' task_label ,'_run-*_channels.tsv']));
+    
+    channelsName = fullfile(D(1).folder, D(1).name);
+    
+    tb_channels = readtable(channelsName,'FileType','text','Delimiter','\t');
+    log_ch_incl = strcmp(tb_channels.type,'ECOG');
+    
+    ch = tb_channels.name;
+    ch_incl = {ch{log_ch_incl}}';
+    
+    data = -1*ccep_data(log_ch_incl,:);
+    
+    dataBase(i).sub_label = sub_labels{i};
+    dataBase(i).ses_label = ses_label;
+    dataBase(i).task_label = task_label;
+    dataBase(i).run_label = run_label;
+    dataBase(i).dataName = dataName;
+    dataBase(i).ccep_header = ccep_header;
+    dataBase(i).tb_events = tb_events;
+    dataBase(i).tb_channels = tb_channels;
+    dataBase(i).tb_electrodes = tb_electrodes;
+    dataBase(i).ch = ch_incl;
+    dataBase(i).data = data;
+    fprintf('...Subject %s has been run...\n',sub_labels{i})
+end
+
+disp('All subjects are loaded')
