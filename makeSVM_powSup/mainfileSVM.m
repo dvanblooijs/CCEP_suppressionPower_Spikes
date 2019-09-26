b4203ea8b7950090dc1d6adfd2ba5162ce0392ed
Dorien van Blooijs <d.vanblooijs@umcutrecht.nl>
2019-09-26 17:55:34 +0200

    restructured repository

--- makeSVM_powSup/mainfileSVM.m
+++
@@ -1,9 +0,0 @@
-%% mainfile SVM_TFSPES
-
-%% process the data to enable construction of TFSPES plots
-
-preprocess_mainfile
-
-%% make SVM
-
-% makeSVM_TFSPES --> needs to be adapted to be BIDS compatible!!
