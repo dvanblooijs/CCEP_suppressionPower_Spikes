e9253574d7f8faf64df1ee62fc0d572119f5cfca
Dorien van Blooijs <d.vanblooijs@umcutrecht.nl>
2019-10-11 13:37:45 +0200

    cleaned up code

--- preprocessing_BBs/makeSVM_powSup/getfeaturesTrain.m
+++ preprocessing_BBs/makeSVM_powSup/getfeaturesTrain.m
@@ -3,27 +3,42 @@
 % made BIDS compatible by Dorien van Blooijs
 % september 2019
 
-function [S,D,A]=getfeaturesTrain(t, allERSP, ThL, ThU, BS_score_M, BS_score_D, stimpchan)
+function [S,D,A]=getfeaturesTrain(subject, ThL, ThU)
+
+t = subject.ERSP.times;
+allERSP = subject.ERSP.allERSPboot;
+BS_score = subject.BS_visscores;
+stimpchan = subject.ERSP.cc_stimsets;
+
+%         Ts=t(2)-t(1);
+%         Fs=1/Ts;
+Ts = median(unique(diff(t)))/1000; % /1000 to convert from ms to s
+Fs = 1/Ts;
 
 ii=1;
- 
+
 for n=1:size(allERSP,1)                            % for each stimulation pair
     for m=1:size(allERSP,2)                        % for each recording electrode
-        ERSP2 = allERSP{n,m};
+        
+        % hysteresis3d assumes non-negative image. We want to detect "blue
+        % blobs" (negative values). Therefor, I multiply the ERSP with -1
+        % and remove all values <0. 
+        ERSP = allERSP{n,m};
+        ERSP2 = -1* ERSP;
+        ERSP2(ERSP2<0) = 0;
+        
+        % delineate blue blobs using hysteresis3d
         t1=ThL;                                          % Lower threshold
-        t2=ThU;                                          % Upper threshold, ook proberen voor 10-15
+        t2=ThU;                                          % Upper threshold
         conn=8;                                             % Connectivity
         [tri,hys]=hysteresis3d(ERSP2,t1,t2,conn);
         allhys{n,m}= hys;
         alltri{n,m}= tri;
-        
+                
         %%Get area and other features
         stats=regionprops(hys,'Area', 'ConvexHull', 'EquivDiameter', 'EulerNumber', 'FilledImage', 'FilledArea', 'Image');
         
         %%Get duration
-        Ts=t(2)-t(1);
-        Fs=1/Ts;
-        
         for i=1:size(stats,1)                               % for each area per picture get duration/area
             img = stats(i).Image;
             for j=1:size(img,1)                             % for each row in the image
@@ -43,8 +58,8 @@ for n=1:size(allERSP,1)                            % for each stimulation pair
         clear ERSP2 hys
         
         %Get indexes of similar scoring
-        if BS_score_D(n,m)==BS_score_M(n,m)
-            S(ii) = BS_score_D(n,m);
+        if BS_score(1,n,m)==BS_score(2,n,m)
+            S(ii) = BS_score(1,n,m);
             
         else
             S(ii) = 0;
